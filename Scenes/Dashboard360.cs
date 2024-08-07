a11ad5e43233e1af9026240a89745b9e36574135
Rocco <73751825+dev-rocco@users.noreply.github.com>
2024-08-07 18:00:25 +0100

    Added multi-page support

--- Scenes/Dashboard360.cs
+++ Scenes/Dashboard360.cs
@@ -4,10 +4,31 @@ using System;
 public partial class Dashboard360 : Control
 {
 	private HBoxContainer _pages;
-	[Export] private int _initialPageIndex = 0; // home page
+	private HBoxContainer _pageNames;
+	[Export] private Control _currentPrimary;
+
+	private Camera2D _cam;
+
+	public void SwitchPrimary(Control newPrimary)
+	{
+		_currentPrimary = newPrimary;
+		_cam.GlobalPosition = _currentPrimary.GetNode<Control>("CameraTarget").GlobalPosition;
+	}
 
 	public override void _Ready()
 	{
 		_pages = GetNode<HBoxContainer>("Pages");
+		_pageNames = GetNode<HBoxContainer>("StaticElements/PageNames");
+
+		foreach (Node i in _pages.GetChildren())
+		{
+			Label newLabel = new Label();
+			newLabel.Text = ((string)i.Name).ToLower();
+			_pageNames.AddChild(newLabel);
+		}
+
+		_cam = GetNode<Camera2D>("Camera2D");
+		_currentPrimary.Call("give_focus");
+		_cam.ResetSmoothing();
 	}
 }
